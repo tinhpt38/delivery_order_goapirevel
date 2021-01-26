14396d0ad9b21a7e6288064f49c6dd105708d323
tinhpt <tinhpt.38@gmail.com>
2021-01-26 18:25:33 +0700

    i do not know

--- app/database/setup.go
+++ app/database/setup.go
@@ -1,30 +1,30 @@
 package database
 
-import (
-	"time"
-	"gopkg.in/mgo.v2"
-)
+// import (
+// 	"time"
+// 	"gopkg.in/mgo.v2"
+// )
 
-var UserCollection *mgo.Collection
-var Session *mgo.Session
+// var UserCollection *mgo.Collection
+// var Session *mgo.Session
 
 
-//Init databse
+// //Init databse
 
-func  InitDB(uri, dbname, user, password string) error{
-	// session, err := mgo.Dial(uri)
-	session, err := mgo.DialWithInfo(&mgo.DialInfo{
-        Addrs:    []string{uri},
-        Username: user,
-        Password: password,
-        Timeout:  100 * time.Second,
-    })
-	if(err != nil){
-		return err 
-	}
+// func  InitDB(uri, dbname, user, password string) error{
+// 	// session, err := mgo.Dial(uri)
+// 	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+//         Addrs:    []string{uri},
+//         Username: user,
+//         Password: password,
+//         Timeout:  100 * time.Second,
+//     })
+// 	if(err != nil){
+// 		return err 
+// 	}
 
-	session.SetMode(mgo.Monotonic,true)
-	Session = session
-	UserCollection = session.DB(dbname).C("user")
-	return nil
-} 
+// 	session.SetMode(mgo.Monotonic,true)
+// 	Session = session
+// 	UserCollection = session.DB(dbname).C("user")
+// 	return nil
+// } 
